66d741c634408961eed58ed06f65c069bcbcab62
Bhavin B <bhavinbhadani@yahoo.in>
2025-01-15 09:26:50 +0530

    Initial Commit

---
+++ Wislist/WislistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WislistApp.swift
+//  Wislist
+//
+//  Created by Bhavin Bhadani on 15/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WislistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
