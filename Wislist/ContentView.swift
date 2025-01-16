a7da4ff5a66d93ffbd7e7fd693f5ad4cfd6a9bf2
Bhavin B <bhavinbhadani@yahoo.in>
2025-01-16 09:33:55 +0530

    Basic add/delete to swift data

--- Wislist/ContentView.swift
+++ Wislist/ContentView.swift
@@ -12,12 +12,50 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var wishes: [Wish]
     
+    @State private var isAlertShowing = false
+    @State private var title = ""
+
     var body: some View {
         NavigationStack {
             List(wishes) { wish in
                 Text(wish.title)
+                    .font(.title.weight(.light))
+                    .padding(.vertical, 2)
+                    .swipeActions {
+                        Button("Delete", role: .destructive) {
+                            modelContext.delete(wish)
+                        }
+                    }
             }
             .navigationTitle("Wishlist")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        isAlertShowing = true
+                    } label: {
+                        Image(systemName: "plus")
+                            .imageScale(.large)
+                    }
+                }
+                
+                if !wishes.isEmpty {
+                    ToolbarItem(placement: .bottomBar) {
+                        Text("\(wishes.count) wish\(wishes.count == 1 ? "" : "es")")
+                    }
+                }
+            }
+            .alert("Create a new wish", isPresented: $isAlertShowing) {
+                TextField("Enter a wish", text: $title)
+                
+                Button {
+                    modelContext.insert(Wish(title: title))
+                    title = ""
+                    
+                    //try! modelContext.save()
+                } label: {
+                    Text("Save")
+                }
+            }
             .overlay {
                 if wishes.isEmpty {
                     ContentUnavailableView(
@@ -31,7 +69,20 @@ struct ContentView: View {
     }
 }
 
-#Preview {
+#Preview("Empty List") {
     ContentView()
         .modelContainer(for: Wish.self, inMemory: true)
+}
+
+#Preview("List with Sample Data") {
+    let container = try! ModelContainer(for: Wish.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    container.mainContext.insert(Wish(title: "Master SwiftData"))
+    container.mainContext.insert(Wish(title: "Buy a new iPhone"))
+    container.mainContext.insert(Wish(title: "Practice latin dances"))
+    container.mainContext.insert(Wish(title: "Travel to Europe"))
+    container.mainContext.insert(Wish(title: "Make positive impact"))
+    
+    return ContentView()
+        .modelContainer(container)
 }
